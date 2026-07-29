86f388533870104fe6e14d8b29c45000b2d8732f
think <lingwh.work@hotmail.com>
2026-07-29 14:42:00 +0800

    修改项目内容

--- 01-jdk-doc/chapter-01-basic/src/main/java/org/bluebridge/basic/MathTest.java
+++ 01-jdk-doc/chapter-01-basic/src/main/java/org/bluebridge/basic/MathTest.java
@@ -13,9 +13,17 @@ public class MathTest {
 
     @Test
     public void testMath() {
+        /**
+         * floorMod：地板取（向负无穷方向取）模，得到余数
+         * floorDiv：地板除法，向地板取（向负无穷方向取）整得到商
+         */
+        // 10÷3≈3.33，向下取整商=3；10 - 3*3 = 1
         log.info("Math.floorMod(10, 3) = {}", Math.floorMod(10, 3));
+        // -10÷3≈-3.33，向下取整商=-4；-10 - (-4*3) = 2
         log.info("Math.floorMod(-10, 3) = {}", Math.floorMod(-10, 3));
+        // 10÷3≈3.33，向下取整，结果3
         log.info("Math.floorDiv(10, 3) = {}", Math.floorDiv(10, 3));
+        // -10÷3≈-3.33，向下取整，结果-4
         log.info("Math.floorDiv(-10, 3) = {}", Math.floorDiv(-10, 3));
         log.info("Math.PI = {}", Math.PI);
     }
